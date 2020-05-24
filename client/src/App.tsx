723511d3be9c2cd22d53146206435bd71e412e4d
Kacper Cierzniewski <kacper.cierzniewski@synergycodes.com>
2020-05-24 17:34:20 +0200

    added calendar library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Navbar } from './components/Navbar';
+import { Calendar } from './pages/Calendar';
 
 export const App = () => {
 	return (
@@ -14,6 +15,7 @@ export const App = () => {
 				<Route exact path="/" component={Home} />
 				<Route exact path="/login" component={Login} />
 				<Route exact path="/register" component={Register} />
+				<Route exact path="/calendar" component={Calendar} />
 			</Container>
 		</Router>
 	);
