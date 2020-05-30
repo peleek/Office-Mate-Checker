32da25cb7d1365c806f0ea06d39e24a1dcd4c8bb
Michał <hortexix@gmail.com>
2020-05-30 17:34:23 +0200

    delete import

--- client/src/pages/LoginAndRegister.tsx
+++ client/src/pages/LoginAndRegister.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { LoginForLandingpage } from './LoginForLandingpage';
-import { Login } from './Login'
 import '../homepage.css';
 
 
