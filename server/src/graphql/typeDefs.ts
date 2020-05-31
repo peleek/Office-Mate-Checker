0f55873ef8ee630587b8e1f34a46b3d5f231f9b4
Kacper <kacper.cierzniewski@synergycodes.com>
2020-05-31 16:27:18 +0200

    linting stuff

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,34 +1,34 @@
 const { gql } = require('apollo-server');
-export const typeDefs =
-    gql`
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
 
-    type User{
-        id: ID!
-        email: String!
-        token: String!
-        username: String!
-        createdAt: String!
-    }
+export const typeDefs = gql`
+	type Post {
+		id: ID!
+		body: String!
+		createdAt: String!
+		username: String!
+	}
 
-    input RegisterInput {
-        username: String!
-        password: String!
-        confirmPassword: String!
-        email: String!
-    }
+	type User {
+		id: ID!
+		email: String!
+		token: String!
+		username: String!
+		createdAt: String!
+	}
 
-    type Query{
-        getPosts: [Post]
-    }
+	input RegisterInput {
+		username: String!
+		password: String!
+		confirmPassword: String!
+		email: String!
+	}
 
-    type Mutation{
-        register(registerInput: RegisterInput): User!
-        login(username: String!, password: String!): User!
-    }
-`
+	type Query {
+		getPosts: [Post]
+	}
+
+	type Mutation {
+		register(registerInput: RegisterInput): User!
+		login(username: String!, password: String!): User!
+	}
+`;
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,18 +1,17 @@
-import { Schema, model, Model, Document } from 'mongoose'
+import { Schema, model, Model, Document } from 'mongoose';
 
 export interface IUserSchema extends Document {
-    username: string,
-    password: string,
-    email: string,
-    createdAt: string,
+	username: string;
+	password: string;
+	email: string;
+	createdAt: string;
 }
 
 const userSchema = new Schema({
-    username: String,
-    password: String,
-    email: String,
-    createdAt: String,
+	username: String,
+	password: String,
+	email: String,
+	createdAt: String,
+});
 
-})
-
-export const UserModel = model<IUserSchema, Model<IUserSchema>>('User', userSchema);    
+export const UserModel = model<IUserSchema, Model<IUserSchema>>('User', userSchema);
