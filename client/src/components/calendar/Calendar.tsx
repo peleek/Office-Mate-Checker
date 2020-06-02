eb9f3166238dc0f2bba747e14ba2b86d5d331ee7
Kacper <kacper.cierzniewski@gmail.com>
2020-06-02 22:31:09 +0200

    checking some calendar events, hold data in one place

--- client/src/components/calendar/Calendar.tsx
+++ client/src/components/calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -6,14 +6,18 @@ import interactionPlugin from '@fullcalendar/interaction';
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/timegrid/main.css';
 
-export const Calendar: React.FC = () => (
-	<FullCalendar
-		events={[
-			{ title: 'event 1', date: Date.now().toLocaleString() },
-			{ title: 'event 2', date: Date.now().toLocaleString() },
-		]}
-		defaultView="timeGridWeek"
-		plugins={[timeGridPlugin, interactionPlugin]}
-		editable
-	/>
-);
+export const Calendar: React.FC = () => {
+	const [events, setEvents] = useState([
+		{ title: 'event 1', start: Date.now(), end: Date.now() },
+		{ title: 'event 2', start: Date.now(), end: Date.now() },
+	]);
+
+	return (
+		<FullCalendar
+			events={events}
+			defaultView="timeGridWeek"
+			plugins={[timeGridPlugin, interactionPlugin]}
+			editable
+		/>
+	);
+};
