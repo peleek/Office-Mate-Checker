7d384cf082b86bbf290c430704423e4b50001f4f
Kacper Cierzniewski <kacper.cierzniewski@synergycodes.com>
2020-08-09 11:20:51 +0200

    mutations adjustments

--- client/src/components/calendar/graphql/eventMutations.ts
+++ client/src/components/calendar/graphql/eventMutations.ts
@@ -3,7 +3,23 @@ import gql from 'graphql-tag';
 export const ADD_EVENT_MUTATION = gql`
 	mutation addEvent($event: EventInput!) {
 		addEvent(event: $event) {
-			eventId
+			description
+		}
+	}
+`;
+
+export const CHANGE_EVENT_MUTATION = gql`
+	mutation changeEvent($event: EventInput!) {
+		changeEvent(event: $event) {
+			description
+		}
+	}
+`;
+
+export const REMOVE_EVENT_MUTATION = gql`
+	mutation removeEvent($eventId: String!) {
+		removeEvent(eventId: $eventId) {
+			description
 		}
 	}
 `;
